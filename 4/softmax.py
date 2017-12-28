9c85dd8b7fd2a86806640f3d8790c8d4ab0e7556
arahtashun <arahtashun@gmail.com>
2017-12-29 01:03:41 +0900

    iadd

--- 4/softmax.py
+++ 4/softmax.py
@@ -5,6 +5,8 @@ datasetの取得と加工には
 https://github.com/oreilly-japan/deep-learning-from-scratch/blob/master/dataset/mnist.py
 を用いた
 """
+import os, sys
+sys.path.append(os.pardir)
 import numpy as np
 from mnist import load_mnist
 
---
+++ 5/feedforward_neural_network.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Author: Shun Arahata
+import os, sys
+sys.path.append(os.pardir)
+import numpy as np
+from mnist import load_mnist
